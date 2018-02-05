aeeb89bd352fa61bbbddad09a84f8b5d21413e54
zerghua <zerghua@gmail.com>
2018-02-05 12:23:56 -0500

    init check in

---
+++ src/BurrowsWheeler.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+/**
+ * Created by HuaZ on 2/5/2018.
+ */
+public class BurrowsWheeler {
+    // apply Burrows-Wheeler transform, reading from standard input and writing to standard output
+    public static void transform(){
+
+    }
+
+    // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
+    public static void inverseTransform(){
+
+    }
+
+    // if args[0] is '-', apply Burrows-Wheeler transform
+    // if args[0] is '+', apply Burrows-Wheeler inverse transform
+    public static void main(String[] args){
+
+    }
+}
